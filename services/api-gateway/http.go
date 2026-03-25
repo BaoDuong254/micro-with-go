66476629d3fdeef2475b5f86ecc76e5d8737a4db
GiaBao <duonggiabao254@gmail.com>
2026-03-25 16:48:09 +0700

    feat: implement CreateTrip RPC and associated request/response messages in trip service

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -8,6 +8,34 @@ import (
 	"ride-sharing/shared/contracts"
 )
 
+func handleTripStart(w http.ResponseWriter, r *http.Request) {
+	var reqBody startTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	tripService, err := grpc_clients.NewTripServiceClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer tripService.Close()
+
+	trip, err := tripService.Client.CreateTrip(r.Context(), reqBody.toProto())
+	if err != nil {
+		log.Printf("Failed to start a trip: %v", err)
+		http.Error(w, "Failed to start trip", http.StatusInternalServerError)
+		return
+	}
+
+	response := contracts.APIResponse{Data: trip}
+
+	writeJSON(w, http.StatusCreated, response)
+}
+
 func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	var reqBody previewTripRequest
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
@@ -22,11 +50,16 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "user ID is required", http.StatusBadRequest)
 		return
 	}
-	
+
+	// Why we need to create a new client for each connection:
+	// because if a service is down, we don't want to block the whole application
+	// so we create a new client for each connection
 	tripService, err := grpc_clients.NewTripServiceClient()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Don't forget to close the client to avoid resource leaks!
 	defer tripService.Close()
 
 	tripPreview, err := tripService.Client.PreviewTrip(r.Context(), reqBody.toProto())
